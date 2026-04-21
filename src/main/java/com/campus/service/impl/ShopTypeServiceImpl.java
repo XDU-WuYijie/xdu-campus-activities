6da4ed7e00a869a713eec681dea7b7bf7d76c0d9
wuyijiedesk <3074740010@qq.com>
2026-04-21 13:36:56 +0800

    fist commit

---
+++ src/main/java/com/campus/service/impl/ShopTypeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.campus.service.impl;
+
+import com.campus.entity.ShopType;
+import com.campus.mapper.ShopTypeMapper;
+import com.campus.service.IShopTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+
+}
